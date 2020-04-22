ff5d229a40f05aed6cb3ab8cc6a50f1e2569c6c5
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-22 14:33:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 def monopoly_with_third_tier
    {:railroads => {
     :pieces => 4,
-    :rent_in_dollars = {
+    :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
@@ -36,7 +36,7 @@ def monopoly_with_fourth_tier
   {
     :railroads => {
     :pieces => 4,
-    :rent_in_dollars = {
+    :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
